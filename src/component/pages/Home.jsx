94768a63650b0f82f9c9a4779721584938bea1d7
iam-gourav <140313751+iam-gourav@users.noreply.github.com>
2024-01-24 17:22:58 +0530

    pagehead

--- src/component/pages/Home.jsx
+++ src/component/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import gsap from 'gsap';
+import bg from '../../images/bg.png'
 import { useGSAP } from '@gsap/react';
 import { useRef } from 'react';
 
@@ -9,39 +10,52 @@ const Home = () => {
   const pText2 = useRef(null)
   const headText = useRef(null)
   const btn = useRef(null)
-  useGSAP(()=>{
-    gsap.from([pText1.current  , headText.current ,pText2.current, btn.current],{
-      opacity:0,
-      y:'100',
-      skewY:10,
-      stagger:.4,
-      delay:1
-    } )
-   
-    
+  useGSAP(() => {
+    gsap.from([pText1.current, headText.current, pText2.current, btn.current], {
+      opacity: 0,
+      y: '100',
+      skewY: 10,
+      stagger: .4,
+      delay: 1
+    })
 
-  } , {scope:container})
+
+
+  }, { scope: container })
   return (
     <div className='homepage'>
-       <div className='w-[100%] h-[600px] flex justify-between  '>
-        <div className="w-[45%] h-[70%] flex flex-col gap-8 justify-center items-start ml-8 ">
-            <p ref={pText1} className='text-xl'>MAKE YOUR PASSION COME TRUE</p>
-            <div ref={headText}>
+      {/* home page */}
+      <div className='w-[100%] h-[600px] flex justify-between bg-white '>
+        <div className="w-[50%] h-[70%] flex flex-col gap-8 justify-center items-start ml-8 mt-8 ">
+          <p ref={pText1} className='text-xl text-[#414141]'>MAKE YOUR PASSION COME TRUE</p>
+          <div ref={headText}>
 
-            <h1 className='text-7xl text-[maroon]'>FIND YOUR</h1>
-            <h1 className='text-7xl text-[maroon]'>IDEAL CAREER</h1>
-            </div>
-            <h2 ref={pText2} className='text-xl'>Are you stuck in a job you don’t enjoy? Not sure which career direction to take? If you want to have a job you are passionate about, well, you can!</h2>
-            <div ref={btn}>
-            <button className='bg-[maroon] text-white mr-4 w-36 h-11 rounded-[30px]'>OUR PROGRAM</button>
-            <button className='bg-[black] text-[maroon] mr-4 w-36 h-11 rounded-[30px]'>ABOUT US</button>
-            </div>
+            <h1 className='text-7xl text-[var(--font-color)]'>FIND YOUR</h1>
+            <h1 className='text-7xl text-[var(--font-color)]'>IDEAL CAREER</h1>
+          </div>
+          <h2 ref={pText2} className='text-xl text-[#414141]'>Are you stuck in a job you don’t enjoy? Not sure which career direction to take? If you want to have a job you are passionate about, well, you can!</h2>
+          <div ref={btn}>
+            <button className='bg-[var(--font-color)] text-white text-l mr-4 w-36 h-11 rounded-[30px]'>OUR PROGRAM</button>
+            <button className='bg-black text-[var(--font-color)] text-l mr-4 w-36 h-11 rounded-[30px]'>ABOUT US</button>
+          </div>
 
         </div>
-        <div className="w-[50%] h-[100%] bg-slate-600"></div>
+        {/* left home section  */}
+        <div className="w-[50%] h-[100%] bg-white  flex justify-center items-center ">
+          {/* <svg viewBox="0 0 250 250" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#FF0066" d="M57,-58.3C70.1,-44,74.2,-22,73.2,-1.1C72.1,19.9,65.8,39.7,52.8,52.1C39.7,64.5,19.9,69.5,-0.4,69.9C-20.6,70.2,-41.2,66,-57.3,53.6C-73.4,41.2,-85,20.6,-82.7,2.3C-80.4,-16,-64.1,-31.9,-48,-46.3C-31.9,-60.6,-16,-73.3,3,-76.3C22,-79.3,44,-72.6,57,-58.3Z" transform="translate(100 100)" />
+          </svg> */}
+          <img className= 'w-[65%] h-[65%] scale-x-100 ' src={bg} alt="" />
+
+        </div>
+      </div>
+      {/*  second page  */}
+      <div>
+
+      </div>
+
     </div>
     
-    </div>
   )
 }
 
